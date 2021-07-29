d62520be3e5e96482d0df927aad6565829be5224
Nicolas <Valente_nicolas@hotmail.com>
2021-07-29 20:40:22 -0300

    que me salga

---
+++ app.js
@@ -0,0 +1,29 @@
+    const express = require('express')
+const { dirname } = require('path')
+const app = express()
+const port = 3030
+const path = require('path')
+
+app.get('/', function (req, res) {
+    res.send('Hola mundo!')
+})
+
+app.get('/home', function(request, response){
+    response.sendFile(path.join(__dirname, 'views/index.html'))
+})
+
+
+app.get('/register', function(request, response) {
+    response.sendFile(path.join(__dirname, 'views/register.html'))
+});
+
+app.use(express.static('public'))
+
+app.get('*', function (request, response){
+    response.send('NOT FOUND', 404)
+})
+
+app.listen(port, ()=>{
+    console.log('La app esta funcionado en http://localhost:'+ port)
+})
+
