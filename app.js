d1df7d84c31e35139a5855b1c9abdd54614813f7
Nicolas <Valente_nicolas@hotmail.com>
2021-07-30 16:25:12 -0300

    sexto

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
-    const express = require('express')
-const { dirname } = require('path')
+const express = require('express')
 const app = express()
-const port = 3030
+const port = process.env.PORT || 3000
 const path = require('path')
-
-app.get('/', function (req, res) {
-    res.send('Hola mundo!')
-})
-
-app.get('/home', function(request, response){
+app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname, 'views/index.html'))
 })
-
-
-app.get('/register', function(request, response) {
-    response.sendFile(path.join(__dirname, 'views/register.html'))
-});
-
 app.use(express.static('public'))
-
-app.get('*', function (request, response){
+app.use(express.static('views'))
+app.get('*', function(request, response) {
     response.send('NOT FOUND', 404)
 })
-
-
-})
 app.listen(port, function() {
-    console.log('Servidor corriendo en el puerto' + port);
+    console.log('Servido corriendo en corriendo en el puerto ' + port  );
 })
+
